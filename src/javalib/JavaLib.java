0867778891a8ba4ed31d27563ad1bf0e2c60dded
jake <20567@my4county.com>
2021-09-03 11:37:32 -0400

    Added more content

--- src/javalib/JavaLib.java
+++ src/javalib/JavaLib.java
@@ -30,24 +30,27 @@ public class JavaLib {
         System.out.println("Enter your first name" );
         firstName = sc.nextLine();
         
-        System.out.println("Great to meet ya, " +firstName +"! I'm Libby, the JavaLib AI! Welcome to the JavaLib! "
-                + "Please give me your age (Don't be shy, I won't tell anyone how old you are :) ) " );
+        System.out.println("Great to meet ya, " +firstName +"! I'm Libby, the JavaLib AI! Welcome to the JavaLib! ");
+        System.out.println("Please give me your age (Don't be shy, I won't tell anyone how old you are :) ) " );
         age = sc.nextLine();
         
         System.out.println("How lovely! Could you please give me an adjective, " +firstName + "? " );
         adjective1 = sc.nextLine();
         
-        System.out.println("Who was your favorite teacher in school?" );
+        System.out.println("Who was your favorite teacher in elementary school?" );
         teacher = sc.nextLine();
         
-        System.out.println("What subject did they teach?" );
+        System.out.println("What subject/grade did they teach?" );
         subject = sc.nextLine();
         
         System.out.println("What is your favorite restaurant?");
         restaurant = sc.nextLine();
         
-        System.out.println("You would never believe what happened to me on my " +age + "th birthday! It was pretty " +adjective1 + "! "
-                + "I went to eat at " +restaurant + ", and I saw my old " +subject +" teacher, " +teacher +"! " +teacher + "smiled and waved hi to me. \"Good to see you," +firstName);
+        System.out.println("You would never believe what happened to me on my " +age + "th birthday! It was pretty " +adjective1 + "! ");
+        System.out.println("I went to eat at " +restaurant + ", and I saw my old " +subject +" teacher, " +teacher +"! ") ;
+        System.out.println( teacher + " smiled and waved hi to me. \"Good to see you, " +firstName + "!\" " +teacher + " said. ");
+        System.out.println("\"It's been so long! What are you doing here?\" " +teacher + " asked. \" It's my birthday!\" I said with a smile.");
+        
     }
     
 }
